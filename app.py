e4e8d8f291471d265585e43fc8623773370c8699
jijiaqiang <ja3592@163.com>
2024-08-23 15:11:20 +0800

    change into glm-4 api, add .env file

--- app.py
+++ app.py
@@ -2,12 +2,18 @@
 from flask import Flask, request, jsonify, render_template, Response
 import requests
 import json
+from dotenv import load_dotenv
 import os
 
 app = Flask(__name__)
 
-# 从配置文件中settings加载配置
-app.config.from_pyfile('settings.py')
+# 从.env文件中加载api配置
+load_dotenv()
+
+# 获取环境变量
+api_url = os.getenv('API_URL')
+api_key = os.getenv('API_KEY')
+
 
 @app.route("/", methods=["GET"])
 def index():
@@ -16,13 +22,10 @@ def index():
 @app.route("/chat", methods=["POST"])
 def chat():
     messages = request.form.get("prompts", None)
-    apiKey = request.form.get("apiKey", None)
-    model = request.form.get("model", "gpt-3.5-turbo")
+    apiKey = api_key
+    model = "glm-4"
     if messages is None:
         return jsonify({"error": {"message": "请输入prompts！", "type": "invalid_request_error", "code": ""}})
-
-    if apiKey is None:
-        apiKey = os.environ.get('OPENAI_API_KEY',app.config["OPENAI_API_KEY"])
 
     headers = {
         "Content-Type": "application/json",
@@ -38,22 +41,21 @@ def chat():
         "max_tokens": 1024,
         "temperature": 0.5,
         "top_p": 1,
-        "n": 1,
         "stream": True,
     }
 
     try:
         resp = requests.post(
-            url=app.config["URL"],
+            url=api_url,
             headers=headers,
             json=data,
             stream=True,
             timeout=(10, 10)  # 连接超时时间为10秒，读取超时时间为10秒
         )
+        print(f'resp:{resp}')
     except requests.exceptions.Timeout:
         return jsonify({"error": {"message": "请求超时，请稍后再试！", "type": "timeout_error", "code": ""}})
 
-    # 迭代器实现流式响应
     def generate():
         errorStr = ""
         for chunk in resp.iter_lines():
@@ -61,24 +63,23 @@ def chat():
                 streamStr = chunk.decode("utf-8").replace("data: ", "")
                 try:
                     streamDict = json.loads(streamStr)  # 说明出现返回信息不是正常数据,是接口返回的具体错误信息
+                    print(f'streamDict:{streamDict}')
                 except:
                     errorStr += streamStr.strip()  # 错误流式数据累加
                     continue
-                delData = streamDict["choices"][0]
-                if delData["finish_reason"] != None :
+                delData = streamDict.get("choices","None")[0]
+                if delData.get("finish_reason",'haha') == 'stop' :
                     break
                 else:
-                    if "content" in delData["delta"]:
-                        respStr = delData["delta"]["content"]
-                        # print(respStr)
-                        yield respStr
+                    respStr = delData["delta"]["content"]
+                    # print(respStr)
+                    yield respStr
 
         # 如果出现错误，此时错误信息迭代器已处理完，app_context已经出栈，要返回错误信息，需要将app_context手动入栈
         if errorStr != "":
             with app.app_context():
                 yield errorStr
-
-    return Response(generate(), content_type='application/octet-stream')
+    return Response(generate(), content_type='text/event-stream')
 
 if __name__ == '__main__':
     app.run(port=5000)
